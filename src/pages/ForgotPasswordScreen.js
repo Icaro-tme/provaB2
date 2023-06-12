500b4be8ab68f42b62de615844eaf944d27da35f
Icaro-tme <icaroabarros@hotmail.com>
2023-06-12 10:59:12 -0300

    Update ForgotPasswordScreen.js

--- src/pages/ForgotPasswordScreen.js
+++ src/pages/ForgotPasswordScreen.js
@@ -2,28 +2,79 @@ import React from 'react';
 import { StyleSheet, Text, View, ScrollView, Button, TextInput } from 'react-native';
 
 const ForgotPasswordScreen = ({ navigation }) => {
-  
-    return (
-      <View style={styles.container}>
-        <Text> AAAAAAAAAAAAAA </Text>
+  const [username, setUsername] = React.useState('');
+  const [senha, setPassword] = React.useState('');
+
+  return (
+    <View style={styles.flex}>
+
+
+    <View style={styles.container}>
+      <Text style={styles.header}>Forgot Password</Text>
+      <Text style={styles.label}>Username</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your username"
+        onChangeText={text => setUsername(text)}
+        defaultValue={username}
+      />
+      <Text style={styles.label}>Senha</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your password"
+        onChangeText={text => setPassword(text)}
+        defaultValue={senha}
+      />
+      <View style={styles.buttonContainer}>
         <Button title="Login" onPress={() => navigation.navigate('Login')} />
       </View>
-    );
-  };
-  
-  const styles = StyleSheet.create({
+    </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+flex:{
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    },
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+    margin:20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderColor: '#888',
+    borderRadius: 10,
     },
-    header: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginVertical: 20,
-    },
-  });
-  
-  export default ForgotPasswordScreen;
-  
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginVertical: 20,
+  },
+  label: {
+    fontWeight: 'bold',
+    alignSelf: 'flex-start',
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#888',
+    borderRadius: 5,
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    color: '#888',
+  },
+  buttonContainer: {
+    width: '100%',
+    marginTop: 20,
+    marginBottom: 10,
+    borderRadius: 10,
+    
+  },
+});
+
+export default ForgotPasswordScreen;
